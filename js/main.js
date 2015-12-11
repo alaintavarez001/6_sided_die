671e3728025a5f82191ab996f33f14ea03e94320
alaintavarez001 <alain.tavarez001@gmail.com>
2015-12-10 21:29:09 -0500

    modified die
    made it a 12 sided die, with rotating animation.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 
 var die = document.getElementById("die");
 die.onclick=function(){
-    var number = Math.round(Math.random()*5+1);
-            var elem = document.getElementById("die_number");
-        elem.innerHTML = number ;
-    }
+    die.src= "img/dodecahedron.gif";
+    var elem = document.getElementById("die_number");
+    elem.innerHTML = "";
+    setTimeout(function(){
+            die.src= "img/dodecahedron-frame1.png";
+            var number = Math.round(Math.random()*11+1);
+            elem.innerHTML = number ;
+    },2000);
+}
+
+
+
